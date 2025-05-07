004649f2c8a0bf2f4dd0333170559004972ef13d
Ashutosh <ashutoshmohanty13703@gmail.com>
2025-05-07 11:37:42 +0530

    button added

--- src/app/(auth)/page.js
+++ src/app/(auth)/page.js
@@ -1,9 +1,14 @@
-
+import Link from "next/link"
 
 function page() {
   return (
-    <div>
-      home
+    <div className="flex items-center pt-50 justify-center">
+    <Link href="/login">
+    <button className="px-8 py-4 bg-green-500 font-bold text-white rounded-md">
+        Login
+    </button>
+    </Link>
+
     </div>
   )
 }
