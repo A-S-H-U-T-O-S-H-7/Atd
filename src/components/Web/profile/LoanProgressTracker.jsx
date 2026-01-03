1fb730833f0bb9e61ada8b36cb6d61d349a8fc11
Ashutosh Mohanty <ashutoshmohanty13703@gmail.com>
2026-01-03 18:07:00 +0530

    tracker

--- src/components/Web/profile/LoanProgressTracker.jsx
+++ src/components/Web/profile/LoanProgressTracker.jsx
@@ -7,7 +7,9 @@ export default function LoanStatusTracker ({ loanStatus = 2 }) {
       case 2: return 'applied';
       case 3: return 'rejected';
       case 6: return 'sanctioned';
-      case 9: return 'disbursed';
+      case 6: return 'sanctioned';
+      case 7: return 'sanctioned';
+      case 8: return 'disbursed';
       case 11: return 'disbursed'; 
       case 12: return 'disbursed'; 
       case 13: return 'closed';
